032b26c435f2a73afa093beb79c461239c7735e5
dkspindola <davi@spindola.de>
2025-01-10 13:39:24 +0100

    Changed parameters fro hyperparameter search to match in thesis

--- lib/functions_CNN_Modelle/model.py
+++ lib/functions_CNN_Modelle/model.py
@@ -3,17 +3,17 @@ from keras import layers
 
 
 # Keras Modell definieren
-def build_model(hp):
+def build_model(hp, window_size, n_features):
     
     # CInputs Layer definieren (10er Window Size, 11 Features), wenn Window Size angepasst wird, hier auch anpassen
-    input_layer = layers.Input(shape=(10,11))
+    input_layer = layers.Input(shape=(window_size,n_features))
     
     # Hyperparameter für die Anzahl an Conv Layer
     num_layers_conv = hp.Int('num_layers_conv', 0, 6)
     print(f'Anzahl an Conv Layers: {num_layers_conv}')
     
     # Hyperparameter für die Lernrate
-    learning_rate = hp.Float('learning_rate', min_value=1e-5, max_value=1e-2, sampling='LOG')
+    learning_rate = hp.Float('learning_rate', min_value=1e-4, max_value=1e-2, sampling='LOG')
     
     # Übergebe den Input Layer als x für die folgende for Schleife
     x = input_layer
@@ -21,7 +21,7 @@ def build_model(hp):
     # Iteriere über die gewählte Anzahl an Conv Schichten
     for i in range(num_layers_conv):
         # Definition weiterer Hyperparameter innerhalb der Schleife für jede Schicht
-        units_conv = hp.Int(f'units_conv{i}', min_value=32, max_value=512, step=32)
+        units_conv = hp.Int(f'units_conv{i}', min_value=1, max_value=15, step=1)
         activation_layer = hp.Choice(f'activation_conv{i}', values =['relu','tanh'])
         kernel_size = hp.Choice(f'kernel_{i}', values = [2,3,4,5])
         l2_regulizer =hp.Float(f'l2_conv{i}', min_value=0.0, max_value=0.01, step=0.001)
@@ -44,7 +44,7 @@ def build_model(hp):
     # Fully Connected Part (MLP) / Dense Schichten
     
     # Definition der Anzahl an Dense Layers
-    num_layers_fully = hp.Int('num_layers_fully', 0,6)
+    num_layers_fully = hp.Int('num_layers_fully', 1,15, 1)
     print(f'Anzahl an Fully Connected Layers: {num_layers_fully}')
 
     # Übergabe der Flatten Schicht
