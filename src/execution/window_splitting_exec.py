cbc6c289da17f47a8ecd39010d0409867382a1a7
Patrick <patrick.reining@outlook.com>
2025-05-10 10:54:32 +0200

    Scaler abspeichern

--- src/execution/window_splitting_exec.py
+++ src/execution/window_splitting_exec.py
@@ -2,6 +2,7 @@ import os
 import json
 from src.process import WindowSplitting
 from src.util import timestamp
+import pickle
 
 class WindowSplittingExecution:
     """Handles the execution of the window splitting process.
@@ -60,4 +61,46 @@ class WindowSplittingExecution:
         folder = os.path.join('build', 'window_split', data_name, timestamp())
         process.save(folder)
         json.dump(metadata, open(os.path.join(folder, 'metadata' + '.json'), 'w'), indent=4)
+        print(f"Data saved in {folder}")
+
+    @classmethod
+    def execute_with_scaler(cls, data_file: str, 
+                     batch_split: bool, 
+                     validation_split: bool, 
+                     test_size: float, 
+                     seed: int, 
+                     batchsize: int,
+                     interpolation: bool,
+                     window_size: int,
+                     sep: str,
+                     decimal: str                  
+                     ):
+        
+        process = WindowSplitting(batch_split, validation_split, interpolation)
+        process.load(data_file, sep, decimal)
+        process.start(test_size, seed, batchsize, window_size)
+
+        _, data_name = os.path.split(data_file)
+        data_name, _ = os.path.splitext(data_name)
+
+        metadata = {
+            "data": data_file, 
+            "seed": int(seed),
+            "test_size": test_size,
+            "window_size": window_size,
+            "validation_split": validation_split,
+            "batch_split": batch_split,
+            "interpolation": interpolation,
+            "batch_size": batchsize
+        }
+
+        folder = os.path.join('build', 'window_split', data_name, timestamp())
+        process.save(folder)
+        json.dump(metadata, open(os.path.join(folder, 'metadata' + '.json'), 'w'), indent=4)
+        print(f"Data saved in {folder}")
+        with open(os.path.join(folder,"scalers_features.pkl"), "wb") as f:
+            pickle.dump(process.scalers_features, f)
+        with open(os.path.join(folder,"scalers_labels.pkl"), "wb") as f:
+            pickle.dump(process.scalers_labels, f)
+        
         print(f"Data saved in {folder}")
